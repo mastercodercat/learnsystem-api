5bb0647cbf2e69138e18ea36b93fd839979535b9
mastercodercat <mastercoderoy64@gmail.com>
2021-11-16 11:41:30 -0800

    fixed: pipeline problem

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -4,6 +4,6 @@ import fs from "fs";
 import config from "../config";
 
 export const getPublicKey = () => {
-  const pathToKey = path.join(__dirname, "../../", config.appPubKey);
+  const pathToKey = path.join(__dirname, "../", config.appPubKey);
   return fs.readFileSync(pathToKey, "utf8");
 };
